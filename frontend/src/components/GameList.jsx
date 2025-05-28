0ffda24c48b03157b1dcbe4fa77a458c096f61f4
Alvin Duran <alvin@fullstacklabs.co>
2025-05-28 03:28:13 -0400

    WIP new UI

---
+++ frontend/src/components/GameList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const GameList = () => {
+    return (
+        <table className="game-list">
+            <thead>
+            <tr>
+                <th>Local</th>
+                <th>Visitante</th>
+                <th>Fecha</th>
+                <th>Resultado</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default GameList;
---
+++ frontend/src/components/TeamList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TeamList = () => {
+    return (
+        <table className="team-list">
+            <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Bandera</th>
+                <th>Fecha creación</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default TeamList;
