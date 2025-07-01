6c723dfcdfc84ef6ebbaec94c32ac4f4cbab542e
Alvin Duran <alvin@fullstacklabs.co>
2025-06-30 22:10:33 -0400

    Build and entrypoint

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 
-python manage.py migrate
+echo "Apply database migrations"
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
+set -o errexit
 
-# Collect static files
-# echo "Collect static files"
-# python manage.py collectstatic --noinput
-
-# Apply database migrations
 echo "Apply database migrations"
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-# Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:10000
