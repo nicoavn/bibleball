30e3e27dbc72231c3b7457cf9fa2adcdfb32572c
Alvin Duran <alvin@fullstacklabs.co>
2025-07-06 17:09:29 -0400

    Entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -o errexit
 
+python manage.py collectstatic --no-input
+
+
 echo "Apply database migrations"
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
